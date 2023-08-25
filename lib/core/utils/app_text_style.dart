c13307ebd0fca3ea48d436bf9a415e8daeb3cf4f
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-08-25 18:45:57 +0300

    app text style

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,35 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle({
+  required Color color,
+  required double fontSize,
+  required FontWeight fontWeight,
+}) {
+  return GoogleFonts.lato(
+      color: color, fontSize: fontSize, fontWeight: fontWeight);
+}
+
+//bold Style
+TextStyle boldStyle({
+  Color color = AppColors.white,
+  double fontSize = 24,
+}) {
+  return _textStyle(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.bold,
+  );
+}
+//regular Style
+TextStyle regularStyle({
+  Color color = AppColors.white,
+  double fontSize = 24,
+}) {
+  return _textStyle(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.normal,
+  );
+}
