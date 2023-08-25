00a756d2f168518b14176c510b0e8dcfbe97e804
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-08-25 18:46:49 +0300

    app text style

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets{
+  static const String basePath = 'assets/images/logo.png';
+  static const String appLogo = '${basePath}logo.png';
+  static const String background = '${basePath}background.png';
+  static const String lock = '${basePath}lock.png';
+  static const String backgroundTwo= '${basePath}background_two.png';
+  static const String imagePicker= '${basePath}image_picker.png';
+}
